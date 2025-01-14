3f059ee0d37eb8de56d9fcc35e29cd536b8f58eb
Jitesh Bagri <144794290+delicate-arc9818@users.noreply.github.com>
2025-01-14 16:33:06 +0000

    dsa in c

--- practice.c
+++ practice.c
@@ -309,3 +309,39 @@
 //     printarray(arr, size);
 //     return 0;
 // }
+
+
+
+
+
+//BUBBLE SORT
+
+
+// void bubblesort(int arr[], int size){
+//     for(int i = 0; i < size - 1; i++){
+//         for(int j = 0; j < size - i; j++){
+//             if(arr[j] > arr[j+1]){
+//                 int temp = arr[j];
+//                 arr[j] = arr[j+1];
+//                 arr[j+1] = temp;
+//             }
+//         }
+//     }
+// }
+// void printarray(int arr[], int size){
+//     for(int i = 0; i < size; i++){
+//         printf("%d ", arr[i]);
+//     }
+// }
+// int main(){
+//     int size;
+//     printf("enter size of array:");
+//     scanf("%d", &size);
+//     int arr[size];
+//     for(int i = 0; i < size; i++){
+//         scanf("%d", &arr[i]);
+//     }
+//     bubblesort(arr, size);
+//     printarray(arr, size);
+//     return 0;
+// }
