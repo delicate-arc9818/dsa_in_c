d6afba3547df2331286f13e3818468d8cb50a5ae
Jitesh Bagri <144794290+delicate-arc9818@users.noreply.github.com>
2025-01-17 05:32:31 +0000

    dsa in c

--- practice.c
+++ practice.c
@@ -375,3 +375,65 @@
 
 
 
+//MERGE SORT
+
+
+// void merge(int arr[], int start, int mid, int end){
+//     int i, j, k;
+//     int n1 = mid - start + 1;
+//     int n2 = end - mid;
+//     int leftarr[n1], rightarr[n2];
+//     for(int i = 0; i < n1; i++){
+//         leftarr[i] = arr[start + i];
+//     }
+//     for(int j = 0; j < n2; j++){
+//         rightarr[j] = arr[mid + 1 + j];
+//     }
+//     i = 0;
+//     j = 0;
+//     k = start;
+//     while(i < n1 && j < n2){
+//         if(leftarr[i] <= rightarr[j]){
+//             arr[k] = leftarr[i];
+//             i++;
+//         }
+//         else{
+//             arr[k] = rightarr[j];
+//             j++;
+//         }
+//         k++;
+//     }
+//     while(i < n1){
+//         arr[k] = leftarr[i];
+//         i++;
+//         k++;
+//     }
+//     while(j < n2){
+//         arr[k] = rightarr[j];
+//         j++;
+//         k++;
+//     }
+  
+// }
+// void mergesort(int arr[], int left, int right){
+//     if(left < right){
+//         int mid = left + (right - left)/2;
+//         mergesort(arr, left, mid);
+//         mergesort(arr, mid+1, right);
+//         merge(arr, left, mid, right);
+//     }
+// }
+// int main(){
+//     int size;
+//     printf("enter size of arr: ");
+//     scanf("%d", &size);
+//     int arr[size];
+//     for(int i  = 0; i < size; i++){
+//         scanf("%d", &arr[i]);
+//     }
+//     mergesort(arr, 0, size-1);
+//     for(int i = 0; i <  size; i++){
+//         printf("%d ", arr[i]);
+//     }
+//     return 0;
+// }
