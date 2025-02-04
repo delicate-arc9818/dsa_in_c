3e8dbefc33177a30945946e6b2a234611e700608
Jitesh Bagri <144794290+delicate-arc9818@users.noreply.github.com>
2025-02-04 08:51:46 +0000

    assignment in c

--- assignment1.c
+++ assignment1.c
@@ -41,20 +41,62 @@
 // }
 
 // int main(){
-//     int arr[7] = {2,4,4,4,6,8,9};
-//     int size = 7;
-//     int target = 6;
+//     int size;
+//     printf("Enter size of array: ");
+//     scanf("%d", &size);
+//     int arr[size];
+//     for(int i = 0; i < size; i++){
+//         scanf("%d", &arr[i]);
+//     }
+//     int target;
+//     printf("Enter target: ");
+//     scanf("%d", &target);
 //     int a1 = first_occurance(arr, size, target);
 //     int a2 = last_occurance(arr, size, target);
 //     printf("{%d %d}\n", a1, a2);
 //     return 0;
 // }
 
+#include <stdio.h>
 
+int findGuardian(int arr[], int start, int end, int round) {
+    if (start == end) {
+        return arr[start];
+    }
 
+    int temp[end - start + 1];
+    int j = 0;
 
-int main(){
-    int size = 7;
-    elimination(size);
+    if (round % 2 == 0) { 
+        for (int i = start; i <= end; i += 2) {
+            temp[j++] = arr[i]; 
+        }
+    } else {  
+        for (int i = end; i >= start; i -= 2) {
+            temp[j++] = arr[i]; 
+        }
+    }
+
+    return findGuardian(temp, 0, j - 1, round + 1);
+}
+
+int main() {
+    int num;
+    printf("Enter number: ");
+    scanf("%d", &num);
+    
+    if (num < 1) {
+        printf("Invalid number of citizens");
+        return 1;
+    }
+
+    int arr[num];
+    for (int i = 0; i < num; i++) {
+        arr[i] = i + 1;
+    }
+
+    int guardian = findGuardian(arr, 0, num - 1, 0);
+    printf("Last remaining citizen: %d", guardian);
+
     return 0;
 }
