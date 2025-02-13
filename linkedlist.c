fa8a769615d848b77830c48f5b9a959b45afe1d8
Jitesh Bagri <144794290+delicate-arc9818@users.noreply.github.com>
2025-02-13 08:26:25 +0000

    linkedlist

--- linkedlist.c
+++ linkedlist.c
@@ -6,6 +6,43 @@ struct Node{
     struct Node* next;
 };
 
+//case-1: insertion at beginning
+struct Node* insertionatfirst(struct Node *head, int data){
+    struct Node* ptr = (struct Node*)malloc(sizeof(struct Node));
+    ptr->data = data;
+    ptr->next = head;
+    return ptr; //ptr becomes the new head
+}
+
+//case-2: insertion in between
+struct Node *insertionatindex(struct Node* head, int data, int index){
+    struct Node *ptr = (struct Node*)malloc(sizeof(struct Node));
+    struct Node *p = head;
+    for(int i = 0; i != index - 1; i++){
+        p = p->next;
+    }
+    ptr->data = data;
+    ptr->next = p->next;
+    p->next = ptr;
+    return head;
+}
+
+
+//case-3: insertion at end
+struct Node *insertatend(struct Node* head, int data){
+    struct Node *ptr = (struct Node*)malloc(sizeof(struct Node));
+    ptr->data = data;
+    struct Node *p = head;
+    for(int i = 0; i != NULL; i++){
+        p = p->next;
+    }
+    p->next = ptr;
+    ptr->next = NULL;
+    return head;
+}
+
+
+
 void linkedlisttraversal(struct Node* ptr){
     while(ptr != NULL){
         printf("Element: %d\n", ptr->data);  //equivalent of (*ptr).data
